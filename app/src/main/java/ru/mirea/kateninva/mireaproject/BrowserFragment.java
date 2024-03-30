a769946636a764b4a24e87ea02aa7d5fc5308d7a
Vladimir Katenin <vkateninr@gmail.com>
2024-03-31 01:57:03 +0300

    Added MireaProject (practice 3)

---
+++ app/src/main/java/ru/mirea/kateninva/mireaproject/BrowserFragment.java
@@ -0,0 +1,75 @@
+package ru.mirea.kateninva.mireaproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BrowserFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BrowserFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BrowserFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BrowserFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BrowserFragment newInstance(String param1, String param2) {
+        BrowserFragment fragment = new BrowserFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private WebView webView;
+    private static final String DEFAULT_URL = "https://mirea.ru";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_browser, container, false);
+
+        webView = rootView.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(DEFAULT_URL);
+
+        return rootView;
+    }
+}
